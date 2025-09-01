f47b07aea6ada2de125016b60ae5c103641d4c2a
SumithBandela <sumithbandela@gmail.com>
2025-09-01 19:21:54 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 👈 enables static export
+  output: "out", // 👈 enables static export
   images: {
     unoptimized: true, // 👈 disables Next image optimizer (works for static hosting)
   },
