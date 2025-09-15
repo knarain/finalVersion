918aa7241cd224b35ae8ff8563a333eff90ab468
SumithBandela <sumithbandela@gmail.com>
2025-09-15 10:00:14 +0530

    commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,7 +12,7 @@ export default function AdminLogin() {
     setError('');
 
     try {
-      const res = await axios.post('http://localhost:8080/api/admin/login', {
+      const res = await axios.post('https://stg.rashmiphotography.com/backend/api/admin/login', {
         username,
         password,
       });
